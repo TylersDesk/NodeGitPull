7dc9214f053cdfc1c68ecb393028165639f093a0
TylersDesk <tymayn@gmail.com>
2014-09-20 12:35:04 -0600

    Added test LS

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+//Some required libs
+var spawn = require('child_process').spawn;
+
 
 /*
  * GET home page.
@@ -6,6 +9,23 @@
 exports.pull = function(req, res){
 	console.log('I had a request. OMG!');
 	console.log(req.body.payload);
+
+	var runGitPull = spawn('ls', [], {
+		cwd: '/opt/project-maynard'
+	});
+
+	runGitPull.stdout.on('data', function(data) {
+		console.log('LS returns:', + data);
+	});
+
+	runGitPull.stderr.on('data', function(data) {
+		console.log('stderr:', + data);
+	});
+
+	runGitPull.on('close', function(code) {
+		console.log('Child Process exetid with code: ', + code);
+	});
+
 	res.json({
 		awesome: "IS HAPPENEING"
 	})
