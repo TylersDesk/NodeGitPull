654baa1f5bfd33a7d41dd18c806711e421ccead9
TylersDesk <tymayn@gmail.com>
2014-09-20 21:54:52 -0600

    Modified server to not require SUDO and to use SSH on git pull

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 //Some required libs
 var exec = require('child_process').exec
+	, whoAmI
 	, runGitPull
 	;
 
@@ -10,6 +11,10 @@ var exec = require('child_process').exec
 exports.pull = function(req, res){
 	console.log('I had a request. OMG!');
 	console.log(req.body.payload);
+
+	whoAmI = exec('whoami', {cdw:"/opt/project-maynard"}, function(error, stdout, stderr){
+		console.log("I am: ", stdout);
+	});
 
 	runGitPull = exec('git pull', {cwd:"/opt/project-maynard"}, function(error, stdout, stderr){
 		console.log("Error: ", error);
